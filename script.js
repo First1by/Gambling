06c4acd840ea267f60afb8f46fa98730101165fd
Sergey <greysound1991@mail.ru>
2023-10-16 23:08:59 +0300

    feat: add burger-menu

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 const burgerMenu = document.querySelector('.header__burger');
 const iconBurger = document.querySelector('.header__burger-menu-icon');
 const closeIconBurger = document.querySelector('.header__burger-icon-close');
-const blackBg = document.querySelector('.div-for-blackout');
+const blackBg = document.querySelector('.wrapper__blackout');
 
 document.onclick = function (e) {
   if (!e.target.closest('.wrapper__header-container')) {
     burgerMenu.classList.remove('header__burger_active');
-    // blackBg.classList.toggle('blackout');
+    blackBg.classList.toggle('wrapper__blackout_active');
   }
 };
 
 iconBurger.onclick = function () {
   burgerMenu.classList.toggle('header__burger_active');
-  // blackBg.classList.toggle('blackout');
+  iconBurger.classList.toggle('header__burger-menu-icon');
+  iconBurger.classList.toggle('header__burger-icon-close');
+  blackBg.classList.toggle('wrapper__blackout_active');
 };
 
 closeIconBurger.onclick = function () {
   burgerMenu.classList.remove('header__burger_active');
-  // blackBg.classList.remove('blackout');
+  blackBg.classList.remove('wrapper__blackout_active');
 };
 
-// blackBg.onclick = function () {
-//   burgerMenu.classList.remove('open-menu');
-//   // blackBg.classList.remove('blackout');
-// };
+blackBg.onclick = function () {
+  burgerMenu.classList.remove('open-menu');
+  burgerMenu.classList.remove('header__burger_active');
+  blackBg.classList.remove('wrapper__blackout_active');
+};
